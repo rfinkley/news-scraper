c08b1c7915d6ad61877e2440bebdfe4cb4102e0b
Robert Finkley <rfinkley82@gmail.com>
2020-04-17 19:39:32 -0400

    added script files for scrape and date functions

---
+++ scripts/scrape.js
@@ -0,0 +1,5 @@
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
