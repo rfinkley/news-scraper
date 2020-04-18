3a55d6fc61ffb957b365b85ff3e7e3ea3e999bde
Robert Finkley <rfinkley82@gmail.com>
2020-04-17 20:22:57 -0400

    completed scrape script

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,5 +1,35 @@
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const scrape = cb => {
+    axios.get("https://www.smashingmagazine.com/articles/").then(res => {
+        const $ = cheerio.load(res.data);
+        let articles = [];
+        $(".article--post").each(function(i, element) {
+            let title = $(this).children(".article--post__title").text().trim();
+            let author = $(this).children(".article--post__author-name").text().trim();
+            let summary = $(this).children(".article--post__teaser").text().trim();
+            let date = $(this).children("time").text().trim();
+
+            if (title && summary) {
+                let titleNeat = title.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+                let summaryNeat = title.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+
+                let data = {
+                    title: title,
+                    author: author,
+                    summary: summary,
+                    date: date
+                };
+
+                articles.push(data);
+            };
+        });
+        cb(articles);
+    });
+};
+
+module.exports = scrape;
