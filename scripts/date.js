301e42260c4bc6a68f1079963b663cfa04042fbf
Robert Finkley <rfinkley82@gmail.com>
2020-04-17 20:23:12 -0400

    completed date format script

---
+++ scripts/date.js
@@ -0,0 +1,12 @@
+const makeDate = () => {
+    let d = new Date();
+    let formattedDate = "";
+
+    formattedDate += (d.getMonth() + 1) + "_";
+    formattedDate += d.getDate() + "_";
+    formattedDate += d.getFullYear();
+
+    return formattedDate;
+}
+
+module.exports = makeDate;
