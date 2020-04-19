72879e24a6a0164925ba383c37bb25e97850cca6
Robert Finkley <rfinkley82@gmail.com>
2020-04-19 11:32:25 -0400

    Changed MongoDB name

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/smashingMagScraper";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true } , err => {
   if (err) {
