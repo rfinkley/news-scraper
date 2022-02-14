d176f3198f26b788af49fc95ca4732804ff081d0
Robert Finkley <me@robertfinkley.com>
2022-02-14 10:41:43 -0500

    updated deprecated modules

--- server.js
+++ server.js
@@ -1,43 +1,48 @@
-var express = require("express");
-var mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Connect to the Mongo DB
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/smashingMagScraper";
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/smashingMagScraper';
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true } , err => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("mongoose connection is successful");
+mongoose.connect(
+  MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('mongoose connection is successful');
+    }
   }
-});
+);
 
 // mongoose.connect("mongodb://localhost/news-scraper", { useNewUrlParser: true });
 
 // Import routes and give the server access to them.
-var router = express.Router();
-require("./config/routes")(router);
+const router = express.Router();
+require('./config/routes')(router);
 
 app.use(router);
 
 // Start the server
-app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
-  });
+app.listen(PORT, function () {
+  console.log('App running on port ' + PORT + '!');
+});
